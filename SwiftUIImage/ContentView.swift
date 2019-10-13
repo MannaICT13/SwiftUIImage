7282606ecc355bab3b0c3aaf586a54e2e583f47f
MannaICT13 <khmannaict13@gmail.com>
2019-10-13 23:10:20 +0600

    Created a Image using SF Symbol

--- SwiftUIImage/ContentView.swift
+++ SwiftUIImage/ContentView.swift
@@ -11,12 +11,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Image("image2")
-        .resizable()
-        .frame(width: 180, height: 180)
-        .clipShape(Circle())
-        .overlay(Circle().stroke(Color.blue,lineWidth: 4))
-        
+        VStack{
+            
+            Image("image2")
+                  .resizable()
+                  .frame(width: 180, height: 180)
+                  .clipShape(Circle())
+                  .overlay(Circle().stroke(Color.blue,lineWidth: 4))
+            
+            Divider()
+                .font(.largeTitle)
+            
+            Image(systemName: "ant.fill")
+                .resizable()
+                .frame(width: 130, height: 130)
+                .background(Color.red)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.black,lineWidth: 4))
+            
+                  
+        }.padding()
+      
     }
 }
 
